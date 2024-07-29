5f1da8bcc4064200b45c9ba9942bf79f008ec2e1
Mega <92765223+Megalaoofania@users.noreply.github.com>
2024-07-29 17:55:41 -0500

    RBXQoL v0.1.10.1 Open Sourcing

---
+++ RBXQoL/css/css5/src/core/addOverlayLayer.js
@@ -0,0 +1,38 @@
+import exitIntro from "./exitIntro";
+import createElement from "../util/createElement";
+import setStyle from "../util/setStyle";
+
+/**
+ * Add overlay layer to the page
+ *
+ * @api private
+ * @method _addOverlayLayer
+ * @param {Object} targetElm
+ */
+export default function addOverlayLayer(targetElm) {
+  const overlayLayer = createElement("div", {
+    className: "introjs-overlay",
+  });
+
+  setStyle(overlayLayer, {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    position: "fixed",
+  });
+
+  targetElm.appendChild(overlayLayer);
+
+  if (this._options.exitOnOverlayClick === true) {
+    setStyle(overlayLayer, {
+      cursor: "pointer",
+    });
+
+    overlayLayer.onclick = () => {
+      exitIntro.call(this, targetElm);
+    };
+  }
+
+  return true;
+}
---
+++ RBXQoL/css/css5/src/core/introForElement.js
@@ -0,0 +1,42 @@
+import addOverlayLayer from "./addOverlayLayer";
+import DOMEvent from "./DOMEvent";
+import { nextStep } from "./steps";
+import onKeyDown from "./onKeyDown";
+import onResize from "./onResize";
+import fetchIntroSteps from "./fetchIntroSteps";
+
+/**
+ * Initiate a new introduction/guide from an element in the page
+ *
+ * @api private
+ * @method introForElement
+ * @param {Object} targetElm
+ * @returns {Boolean} Success or not?
+ */
+export default function introForElement(targetElm) {
+  if (this._introStartCallback !== undefined) {
+    this._introStartCallback.call(this, targetElm);
+  }
+
+  //set it to the introJs object
+  const steps = fetchIntroSteps.call(this, targetElm);
+
+  if (steps.length === 0) {
+    return false;
+  }
+
+  this._introItems = steps;
+
+  //add overlay layer to the page
+  if (addOverlayLayer.call(this, targetElm)) {
+    //then, start the show
+    nextStep.call(this);
+
+    if (this._options.keyboardNavigation) {
+      DOMEvent.on(window, "keydown", onKeyDown, this, true);
+    }
+    //for window resize
+    DOMEvent.on(window, "resize", onResize, this, true);
+  }
+  return false;
+}
